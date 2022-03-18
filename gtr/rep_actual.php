45ba96df25a0dfae40b0b4f3f681727481a9e4c7
D4rksit3 <gonzaloroque21@gmail.com>
2022-03-18 16:46:13 -0500

    fin

--- gtr/rep_actual.php
+++ gtr/rep_actual.php
@@ -102,10 +102,18 @@ https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+ <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script> 
+
 
 <script type="text/javascript">
     $(document).ready(function() {
         $('#customersTable').dataTable({
+            dom: 'Bfrtip',
+            buttons: [ 'excel','print' ],
             "processing": true,
             "ajax": "get_fecha.php",
             "columns": [
